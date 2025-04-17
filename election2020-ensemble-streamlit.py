9916a0e687e0084c9f47d064502709fe667ef90e
iamquantumis <160371563+iamquantumis@users.noreply.github.com>
2025-04-16 21:41:04 -0700

    Minor fixes

--- election2020-ensemble-streamlit.py
+++ election2020-ensemble-streamlit.py
@@ -286,7 +286,8 @@ def main():
         cand2_df['candidate'] = candidate2_name
 
         # Merge the two dataframes
-        merged_df = pd.concat([cand1_df, cand2_df]).reset_index(drop=True)
+        merged_df = pd.concat([cand1_df, cand2_df], ignore_index=True)
+        merged_df = merged_df.loc[:, ~merged_df.columns.duplicated()] # Drop duplicate columns
 
         st.write("### Sampled Merged Data Preview")
         st.dataframe(merged_df.sample(10))
